7cc82c8ac3f7ca0f703bed7285ea20f1f2ef562d
Eddie Welker <eddie.welker@gmail.com>
2010-05-09 16:26:03 -0400

    settings update

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/home/ewelker/git_repos/columbia_orchestra/orch.db'             
+DATABASE_NAME = '/home/ewelker/git_repos/columbia_orchestra/orch/orch.db'             
 # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
