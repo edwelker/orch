5e4ca4c1ff778370f26b8d2cc94bed4c0b6c631f
Edward Welker <edwelker@prokofiev.home>
2010-05-15 16:54:20 -0400

    fixed caching for members and events

--- homepage_viewer/views.py
+++ homepage_viewer/views.py
@@ -8,35 +8,55 @@ from django.core.cache import cache
 
 # Create your views here.
 
+CACHE_TIME = 3600
+
 def home(request):
     '''get an event and member to display on the homepage. objects cached for an hour'''
     
-    member = cache.get('member')
-    primary_event = cache.get('primary_event')
-    secondary_event = cache.get('secondary_event')
+    m = get_member()
+    p = get_primary()
+    s = get_secondary()
     
-    if member and primary_event and secondary_event:
-        return render_to_response('home.html', {'member': member, 'primary_event': primary_event, 'secondary_event': secondary_event}, 
+    return render_to_response('home.html', {'member': m, 'primary_event': p, "secondary_event": s}, 
                               context_instance=RequestContext(request))
-    
+   
+
+def get_member():
+    member = cache.get('member')
+
+    if member:
+        return member
+
     m = OrchestraMember.objects.exclude(photo__exact="").exclude(noncurrent_member=True).exclude(bio__exact="")
     if len(m) > 0:
         rand_index = random.randint(0, (len(m) -1 ))
         member = m[rand_index] if (len(m) > 0) else None
     
-    #Status=1: Primary Event
-    e = Event.objects.filter(date__gte=datetime.datetime.today()).filter(status=1).order_by("date")
+    cache.set('member', member, CACHE_TIME )
+
+    return member
+
+def get_primary():
+    primary_event = cache.get('primary_event')
+    
+    if primary_event:
+        return primary_event
+
+    e = Event.objects.filter(date__gte=datetime.datetime.today()).filter(status=1).order_by('date')
     primary_event = e[0] if (len(e) > 0) else None
+
+    cache.set('primary_event', primary_event, CACHE_TIME )
+    return primary_event
+
+def get_secondary():
+    secondary_event = cache.get('secondary_event')
     
-    #Status=2: Secondary Event
-    ee = Event.objects.filter(date__gte=datetime.datetime.today()).filter(status=2).order_by("date")
-    secondary_event = ee[0] if (len(ee) > 0) else None
-    
-    #set for the hour
-    cache.set('member', member, 3600)
-    cache.set('primary_event', primary_event, 3600)
-    cache.set('secondary_event', secondary_event, 3600)
-    
-    return render_to_response('home.html', {'member': member, 'primary_event': primary_event, "secondary_event": secondary_event}, 
-                              context_instance=RequestContext(request))
-    
+    if secondary_event:
+        return secondary_event
+
+    e = Event.objects.filter(date__gte=datetime.datetime.today()).filter(status=2).order_by('date')
+    secondary_event = e[0] if (len(e) > 0 ) else None
+
+    cache.set('secondary_event', secondary_event, CACHE_TIME )
+    return secondary_event
+
