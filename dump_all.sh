aed10260272687d285a110b620efcac205d0082e
Edward Welker <eddie.welker@gmail.com>
2010-05-31 17:13:42 -0400

    fix for dump data script

--- dump_all.sh
+++ dump_all.sh
@@ -3,7 +3,7 @@ python manage.py dumpdata --indent 2 events > events/fixtures/initial_data.json;
 python manage.py dumpdata --indent 2 pieces > pieces/fixtures/initial_data.json;
 python manage.py dumpdata --indent 2 sponsors > sponsors/fixtures/initial_data.json;
 python manage.py dumpdata --indent 2 flatpages > flatpages/fixtures/initial_data.json;
-python manage.py dumpdata --indent 2 soloists > flatpages/fixtures/initial_data.json;
+python manage.py dumpdata --indent 2 soloists > soloists/fixtures/initial_data.json;
 echo "all fixtures dumped";
 
 python manage.py update_index;
