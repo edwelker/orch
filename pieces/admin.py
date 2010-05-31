e505c4a7bc6cf1ea0e7d654e17330ee78dc05be8
Edward Welker <eddie.welker@gmail.com>
2010-05-31 13:24:59 -0400

    fixtures for pieces, last two years added

--- pieces/admin.py
+++ pieces/admin.py
@@ -1,19 +1,21 @@
 from orch.pieces.models import Composer, Piece
 from django.contrib import admin
 
-class ComposerInline(admin.StackedInline):
-    model = Composer
-
-class PieceAdmin(admin.ModelAdmin):
-    inlines = [ ComposerInline, ]
+class PieceInline(admin.StackedInline):
+    model = Piece 
+    extra = 1
 
 class ComposerAdmin(admin.ModelAdmin):
-#    fieldsets = (
-#            ("Standard Composer", { 'fields': (('first_name', 'middle_name', 'last_name'), 'url')}),
-#            (None, { 'fields': ('other_composer')}),
-#            )
-    pass
+    inlines = [ PieceInline, ]
+
+    fieldsets = (
+            ("Standard Composer", { 'fields': (('first_name', 'middle_name', 'last_name'), 'url')}),
+            ("Advanced", { 'classes' : ('collapse',), 'fields': ('other_composer',)}),
+            )
+    list_display = ('__unicode__',)
+    search_fields = ('last_name', 'first_name', 'middle_name', 'other_composer',)
+    list_per_page = 50
 
 admin.site.register(Composer, ComposerAdmin)
-admin.site.register(Piece, PieceAdmin)
+admin.site.register(Piece)
 
--- pieces/models.py
+++ pieces/models.py
@@ -3,14 +3,25 @@ from django.db import models
 # Create your models here.
 class Piece(models.Model):
     title = models.CharField(max_length=250)
-    movement = models.CharField(max_length=100, help_text="Optional")
+    movement = models.CharField(blank=True,null=True,max_length=100, help_text="Optional")
     composer = models.ForeignKey("Composer")
+    def __unicode__(self):
+        return "%s" % self.title
 
 class Composer(models.Model):
     first_name = models.CharField(blank=True,null=True,max_length=100)
     middle_name = models.CharField(blank=True,null=True,max_length=100)
     last_name = models.CharField(blank=True,null=True,max_length=100)
 
-    other_composer = models.CharField(blank=True,null=True,max_length=100, help_text="For cases where composer is 'Traditional' or something like that")
+    other_composer = models.CharField(blank=True,null=True,max_length=100, help_text="For cases where composer is 'Traditional' or something not following the First Middle Last name pattern.")
 
     url = models.URLField(blank=True,null=True)
+
+    class Meta:
+        ordering = ["last_name"]
+
+    def __unicode__(self):
+        if self.other_composer:
+            return "%s" % self.other_composer
+        else:
+            return "%s, %s %s" % (self.last_name, self.first_name, self.middle_name)
