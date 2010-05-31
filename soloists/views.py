8d428061d49672e583ce9607ae29254cdf9e41b5
Edward Welker <eddie.welker@gmail.com>
2010-05-31 19:37:19 -0400

    many changes to soloist views to get them working again

--- soloists/views.py
+++ soloists/views.py
@@ -4,15 +4,31 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 # Create your views here.
 
-#NEED: to be fixed so that soloist_events and season_soloists are in one iterable list.    
 def all_soloists(request):
     curSeason = get_current_season()
-    season_soloists = Soloist.objects.filter(season=curSeason).order_by('event')
-    return render_to_response('soloists.html', { 'soloists': season_soloists, 'season': curSeason })
+    
+    events = curSeason.event_set.all()
+    #get all of the pieces in events if there are any
+    pieces = [event.pieces.all() for event in events if event.pieces]
+    
+    #unpack the subarrays
+    pieces = [piece for subarray in pieces for piece in subarray]
+
+    #get all of the soloists for pieces if there are any
+    soloists = [piece.soloist.all() for piece in pieces if piece.soloist.all()]
+
+    #unpack the subarrays again
+    soloists = [soloist for subarray in soloists for soloist in subarray]
+    
+    return render_to_response('soloists.html', { 'soloists': soloists, 'season': curSeason })
     
 def single_soloist(request, slug):
     soloist = get_object_or_404(Soloist, slug=slug)
-    #array of events... becuase Soloists can do more than one concert
-    events = soloist.event_set.all()
-    return render_to_response('single_soloist.html', { 'soloist': soloist, 'events': events })
- 
+    
+    pieces = soloist.piece_set.all()
+    events = [piece.event_set.all() for piece in pieces] #get 'em
+    events = [event for subarray in events for event in subarray] #unpack
+
+    #seasons = [event.season for event in events]
+    
+    return render_to_response('single_soloist.html', { 'soloist': soloist, 'pieces': pieces, 'events': events })
